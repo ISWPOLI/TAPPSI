226c73bde1aef28ba9ad19a744e6be4caa04f460
Pablo Vallejo <pabloninety@gmail.com>
2017-05-10 07:44:14 -0500

    Initialize map

--- mapa-de-ruta/app.js
+++ mapa-de-ruta/app.js
@@ -1,5 +1,5 @@
 (function() {
-
+	console.log('HERE')
   mapboxgl.accessToken = 'pk.eyJ1IjoicGFibG92YWxsZWpvIiwiYSI6ImNpdjU1OHhkYzAxM3oyem55NnoxeWxuZXgifQ.ASHXXLrQFb7uauLJ0c8qAg';
   var map = new mapboxgl.Map({
     container: 'map',
