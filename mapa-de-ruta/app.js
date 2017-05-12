b9389690f141a454bc2e5168df9ab22d287b752f
Pablo Vallejo <pabloninety@gmail.com>
2017-05-11 21:07:34 -0500

    Add markers

--- mapa-de-ruta/app.js
+++ mapa-de-ruta/app.js
@@ -1,9 +1,49 @@
 (function() {
-	console.log('HERE')
+
   mapboxgl.accessToken = 'pk.eyJ1IjoicGFibG92YWxsZWpvIiwiYSI6ImNpdjU1OHhkYzAxM3oyem55NnoxeWxuZXgifQ.ASHXXLrQFb7uauLJ0c8qAg';
   var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9'
+    style: 'mapbox://styles/mapbox/streets-v9',
+    zoom: 15
+  });
+
+  map.setCenter([-74.0720920, 4.7109890]);
+
+  map.on('load', function () {
+
+	   // Add driver icon.
+	   map.addSource('point', {
+	      "type": "geojson",
+	      "data": {
+	          "type": "Point",
+	          "coordinates": [
+	              -74.0720920, 4.7109890
+	          ]
+	      }
+	   });
+
+	   map.addLayer({
+	     "id": "point",
+	     "source": "point",
+	     "type": "circle",
+	     "paint": {
+	        "circle-radius": 6,
+	          "circle-radius-transition": {duration: 0},
+	          "circle-opacity-transition": {duration: 0},
+	          "circle-color": "#007cbf"
+	      }
+	  });
+
+	  map.addLayer({
+	      "id": "point1",
+	      "source": "point",
+	      "type": "circle",
+	      "paint": {
+	          "circle-radius": 6,
+	          "circle-color": "#007cbf"
+	      }
+	  });
+
   });
 
 })();
